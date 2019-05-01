940174dad21767cc1841d2fea0e2513bea45312e
Ralph Wiedemeier <ralph@framefactory.ch>
2019-05-01 11:37:01 +0200

    updated server config

--- source/server/ExpressServer.ts
+++ source/server/ExpressServer.ts
@@ -108,13 +108,14 @@ export default class ExpressServer
         const port = this.config.port;
 
         return new Promise((resolve, reject) => {
-            this.server.listen(port, err => {
-                if (err) {
-                    return reject(new Error(err));
-                }
-
-                return resolve();
-            });
+            try {
+                this.server.listen(port, () => {
+                    return resolve();
+                });
+            }
+            catch (err) {
+                return reject(err);
+            }
         });
     }
 
