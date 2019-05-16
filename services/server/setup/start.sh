61baaeaade247a863f5857e15721a19782066db4
Ralph Wiedemeier <ralph@framefactory.ch>
2019-05-16 13:17:34 +0200

    updated dependencies

--- services/server/setup/start.sh
+++ services/server/setup/start.sh
@@ -9,8 +9,15 @@ source /root/.nvm/nvm.sh
 cd /app
 npm install
 
-# build client and server code
-npm run build
+# build server code in services/server/bin/
+if [ ! -d "services/server/bin" ]; then
+    npm run build-server
+fi
+
+# build development client code in dist/
+if [ ! -d "dist" ]; then
+    npm run build-dev
+fi
 
 # start server in debug mode, watching source code changes
 npm run watch
