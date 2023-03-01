b8c92e803b7bfec441bc6fdd6580fec609109db9
Sarah MENOUAR <s.menouar@holusion.com>
2023-03-01 15:57:06 +0100

    add login form

--- source/holusion/client/SplitUserInterface/SettingsView.ts
+++ source/holusion/client/SplitUserInterface/SettingsView.ts
@@ -10,6 +10,9 @@ export default class SettingsView extends DocumentView
   @property({type: Boolean })
   isOpen: boolean = false;
 
+  @property({type: Boolean })
+  isLogin: boolean = false;
+
   protected get viewer()
   {
       return this.system.getComponent(CVViewer, true);
@@ -37,9 +40,43 @@ export default class SettingsView extends DocumentView
 
   protected render()
   {
+
     const filesList = this.files?
-     html`${this.files.map(file=> html`<ff-button icon="file" text=${file}></ff-button>`)}`:
-     html`Aucun fichier trouvé`;
+      html`${this.files.map(file=> html`<ff-button icon="file" text=${file}></ff-button>`)}`:
+      html`Aucun fichier trouvé`;
+
+    const updateFiles = html`<div>
+      <h2>Synchroniser mes données</h2>
+      <ff-button class="btn-primary" text="Mettre à jour les scènes"></ff-button>
+    </div>`
+
+    const loginForm = html`<div class= "login-form">
+        <h2>Me connecter</h2>
+        <form id="userlogin" class="form-control form-modal" @submit=${this.onLoginSubmit}>
+          <div class="form-group">
+            <div class="form-item">
+              <input type="text" autocomplete="username" name="username" id="username" placeholder="username" required>
+              <label for="username">Nom d'utilisateur</label>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="form-item">
+              <input type="password" autocomplete="current-password" name="password" id="password" placeholder="mot de passe" required>
+              <label for="password">Mot de passe</label>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="form-item">
+              <input class="ff-button ff-control btn-primary" type="submit" value="Me connecter" >
+            </div>
+          </div>
+        </form>
+      </div>`
+
+    const logout = html`<div>
+        <h2>Me déconnecter</h2>
+        <ff-button text="Me déconnecter" @click=${this.onLogout}></ff-button>
+      </div>`
 
     if(!this.isOpen){
       return html`<ff-button class="open-btn" @click=${this.onOpen} icon="cog"></ff-button>`
@@ -47,10 +84,13 @@ export default class SettingsView extends DocumentView
     return html`<div class="settings">
       <ff-button class="open-btn" style="position:absolute; right:0; top:0" @click=${this.onClose} icon="close"></ff-button>
       <h1>Paramètres</h1>
-      <h2>Télécharger mes scènes</h2>
+      ${!this.isLogin? loginForm : updateFiles}
+      <h2>Télécharger mes scènes locales</h2>
       <div class="files-list">
         ${filesList}
       </div>
+      ${this.isLogin? logout : ""}
+
     </div>`;
   }
 
@@ -63,4 +103,11 @@ export default class SettingsView extends DocumentView
     this.classList.add("visible")
   }
 
+  onLoginSubmit = ()=>{
+    this.isLogin = true;
+  }
+
+  onLogout = ()=>{
+    this.isLogin = false;
+  }
 }
