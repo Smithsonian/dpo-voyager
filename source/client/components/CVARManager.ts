8572b0d964c8dd4c6b73941e4c44e491bf7c6531
Jamie Cope <jamie@gcope.com>
2024-02-27 12:08:29 -0500

    Cleanup debug statements

--- source/client/components/CVARManager.ts
+++ source/client/components/CVARManager.ts
@@ -113,8 +113,8 @@ export default class CVARManager extends Component
         // SI-specific to return QR codes for legacy content
         const docUri = document.documentURI;
         if(this._arCodeImage == null && docUri.includes("3d-api.si.edu")) {
-            const uuid = docUri.split("/").pop().split(":").pop(); console.log(uuid);
-            this._arCodeImage = "https://3d-api.si.edu/voyager/" + uuid + "/qrcode"; console.log(this._arCodeImage);
+            const uuid = docUri.split("/").pop().split(":").pop();
+            this._arCodeImage = "https://3d-api.si.edu/voyager/" + uuid + "/qrcode";
         }
 
         return this._arCodeImage;
