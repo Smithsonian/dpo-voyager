4d5cdb2772106c562ce923dc52753ed4bcfea0b2
Sebastien DUMETZ <s.dumetz@holusion.com>
2024-04-15 15:41:58 +0200

    handle double-click pivot move

--- source/client/ui/SceneView.ts
+++ source/client/ui/SceneView.ts
@@ -73,6 +73,7 @@ export default class SceneView extends SystemView
         this.ownerDocument.addEventListener("pointermove", this.manipTarget.onPointerMove);         // To catch out of frame drag releases
         this.ownerDocument.addEventListener("pointerup", this.onPointerUpOrCancel);     // To catch out of frame drag releases
         this.ownerDocument.addEventListener("pointercancel", this.onPointerUpOrCancel); // To catch out of frame drag releases
+        this.addEventListener("dblclick", this.manipTarget.onDoubleClick);
         this.addEventListener("wheel", this.manipTarget.onWheel);
         this.addEventListener("contextmenu", this.manipTarget.onContextMenu);
         this.addEventListener("keydown", this.manipTarget.onKeyDown);
@@ -180,6 +181,7 @@ export default class SceneView extends SystemView
             this.ownerDocument.addEventListener("pointermove", this.manipTarget.onPointerMove);         // To catch out of frame drag releases
             this.ownerDocument.addEventListener("pointerup", this.onPointerUpOrCancel);     // To catch out of frame drag releases
             this.ownerDocument.addEventListener("pointercancel", this.onPointerUpOrCancel); // To catch out of frame drag releases
+            this.addEventListener("dblclick", this.manipTarget.onDoubleClick);
             this.addEventListener("wheel", this.manipTarget.onWheel);
             this.addEventListener("contextmenu", this.manipTarget.onContextMenu);
             this.addEventListener("keydown", this.manipTarget.onKeyDown);
@@ -198,6 +200,7 @@ export default class SceneView extends SystemView
             this.ownerDocument.removeEventListener("pointermove", this.manipTarget.onPointerMove);         // To catch out of frame drag releases
             this.ownerDocument.removeEventListener("pointerup", this.onPointerUpOrCancel);     // To catch out of frame drag releases
             this.ownerDocument.removeEventListener("pointercancel", this.onPointerUpOrCancel); // To catch out of frame drag releases
+            this.removeEventListener("dblclick", this.manipTarget.onDoubleClick);
             this.removeEventListener("wheel", this.manipTarget.onWheel);
             this.removeEventListener("contextmenu", this.manipTarget.onContextMenu);
             this.removeEventListener("keydown", this.manipTarget.onKeyDown);
