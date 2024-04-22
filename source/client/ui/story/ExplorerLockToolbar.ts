a85b7a03f98c6eb2cf106901a5a15d7b860cdc00
Jamie Cope <jamie@gcope.com>
2024-04-22 15:18:04 -0400

    Copyright update

--- source/client/ui/story/ExplorerLockToolbar.ts
+++ source/client/ui/story/ExplorerLockToolbar.ts
@@ -1,6 +1,6 @@
 /**
  * 3D Foundation Project
- * Copyright 2022 Smithsonian Institution
+ * Copyright 2024 Smithsonian Institution
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
