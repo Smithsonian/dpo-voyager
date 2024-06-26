f764fb72e764ee275f76d2bdd69655140e6732a4
Sebastien DUMETZ <s.dumetz@holusion.com>
2024-06-26 14:20:38 +0200

    remove usage of @ff/browser/parseUrlParameter in favor of native URLSearchParams object

--- source/client/applications/ExplorerApplication.ts
+++ source/client/applications/ExplorerApplication.ts
@@ -15,8 +15,6 @@
  * limitations under the License.
  */
 
-import parseUrlParameter from "@ff/browser/parseUrlParameter";
-
 import TypeRegistry from "@ff/core/TypeRegistry";
 
 import Notification from "@ff/ui/Notification";
@@ -265,24 +263,24 @@ Version: ${ENV_VERSION}
     {
         const props = this.props;
         const manager = this.assetManager;
-
-        props.root = props.root || parseUrlParameter("root") || parseUrlParameter("r");
-        props.dracoRoot = props.dracoRoot || parseUrlParameter("dracoRoot") || parseUrlParameter("dr");
-        props.resourceRoot = props.resourceRoot || parseUrlParameter("resourceRoot") || parseUrlParameter("rr");
-        props.document = props.document || parseUrlParameter("document") || parseUrlParameter("d");
-        props.model = props.model || parseUrlParameter("model") || parseUrlParameter("m");
-        props.geometry = props.geometry || parseUrlParameter("geometry") || parseUrlParameter("g");
-        props.texture = props.texture || parseUrlParameter("texture") || parseUrlParameter("t");
-        props.occlusion = props.occlusion || parseUrlParameter("occlusion") || parseUrlParameter("o");
-        props.normals = props.normals || parseUrlParameter("normals") || parseUrlParameter("n");
-        props.quality = props.quality || parseUrlParameter("quality") || parseUrlParameter("q");
-        props.uiMode = props.uiMode || parseUrlParameter("uiMode") || parseUrlParameter("u");
-        props.bgColor = props.bgColor || parseUrlParameter("bgColor") || parseUrlParameter("bc");
-        props.bgStyle = props.bgStyle || parseUrlParameter("bgStyle") || parseUrlParameter("bs");
-        props.controls = props.controls || parseUrlParameter("controls") || parseUrlParameter("ct");
-        props.prompt = props.prompt || parseUrlParameter("prompt") || parseUrlParameter("pm");
-        props.reader = props.reader || parseUrlParameter("reader") || parseUrlParameter("rdr");
-        props.lang = props.lang || parseUrlParameter("lang") || parseUrlParameter("l");
+        const qs = new URL(window.location.href).searchParams;
+        props.root = props.root || qs.get("root") || qs.get("r");
+        props.dracoRoot = props.dracoRoot || qs.get("dracoRoot") || qs.get("dr");
+        props.resourceRoot = props.resourceRoot || qs.get("resourceRoot") || qs.get("rr");
+        props.document = props.document || qs.get("document") || qs.get("d");
+        props.model = props.model || qs.get("model") || qs.get("m");
+        props.geometry = props.geometry || qs.get("geometry") || qs.get("g");
+        props.texture = props.texture || qs.get("texture") || qs.get("t");
+        props.occlusion = props.occlusion || qs.get("occlusion") || qs.get("o");
+        props.normals = props.normals || qs.get("normals") || qs.get("n");
+        props.quality = props.quality || qs.get("quality") || qs.get("q");
+        props.uiMode = props.uiMode || qs.get("uiMode") || qs.get("u");
+        props.bgColor = props.bgColor || qs.get("bgColor") || qs.get("bc");
+        props.bgStyle = props.bgStyle || qs.get("bgStyle") || qs.get("bs");
+        props.controls = props.controls || qs.get("controls") || qs.get("ct");
+        props.prompt = props.prompt || qs.get("prompt") || qs.get("pm");
+        props.reader = props.reader || qs.get("reader") || qs.get("rdr");
+        props.lang = props.lang || qs.get("lang") || qs.get("l");
 
         const url = props.root || props.document || props.model || props.geometry;
         this.setBaseUrl(new URL(url || ".", window.location as any).href);
