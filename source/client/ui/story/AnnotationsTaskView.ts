Smithsonian/dpo-voyager

commit 69b59c895e63aa0a608cd374880b78c45d1e5f2b

remove AudioTask's local language setting

source/client/ui/story/AudioTaskView.ts
@@ -62,6 +62,7 @@ export default class AudioTaskView extends TaskView<CVAudioTask>
         }
         
         const ins = this.task.ins;
+        const languageManager = this.activeDocument.setup.language;
 
         const narrationFlagClass = "sv-task-option-base-align";
         const audio = this.task.audioManager;
@@ -71,7 +72,7 @@ export default class AudioTaskView extends TaskView<CVAudioTask>
 
         const detailView = audioElement ? html`<div class="ff-scroll-y ff-flex-column sv-detail-view">
             <sv-property-view .property=${ins.title}></sv-property-view>
-            <sv-property-view .property=${ins.language}></sv-property-view>
+            <sv-property-view .property=${languageManager.ins.language}></sv-property-view>
             <div class="sv-indent">
                 <sv-property-view id="filename" .property=${ins.filepath} @drop=${this.onDropFile} @dragenter=${this.onDragEnter} @dragover=${this.onDragOver} @dragleave=${this.onDragLeave}></sv-property-view>
                 <sv-property-view id="captionfile" .property=${ins.captionPath} @drop=${this.onDropFile} @dragenter=${this.onDragEnter} @dragover=${this.onDragOver} @dragleave=${this.onDragLeave}></sv-property-view>

source/client/components/CVAudioTask.ts
@@ -44,7 +44,6 @@ export default class CVAudioTask extends CVTask
         filepath: types.String("Audio.Filepath", null),
         captionPath: types.String("Audio.CaptionPath", null),
         isNarration: types.Boolean("Audio.IsNarration", false),
-        language: types.Option("Task.Language", Object.keys(ELanguageStringType).map(key => ELanguageStringType[key]), ELanguageStringType[ELanguageType[DEFAULT_LANGUAGE]]),
     };
 
     protected static readonly outs = {
@@ -83,8 +82,6 @@ export default class CVAudioTask extends CVTask
         //this.nodeProvider.activeNode = this.nodeProvider.scopedNodes[0];
         
         super.activateTask();
-
-        this.synchLanguage();
     }
 
     deactivateTask()
@@ -103,13 +100,7 @@ export default class CVAudioTask extends CVTask
 
         const clip = audioManager.getAudioClip(ins.activeId.value);
         const languageManager = this.activeDocument.setup.language;
-
-        if(ins.language.changed) {   
-            const newLanguage = ELanguageType[ELanguageType[ins.language.value]];
-
-            languageManager.addLanguage(newLanguage);  // add in case this is a currently inactive language
-            languageManager.ins.language.setValue(newLanguage);
-        }
+        const activeLanguage = languageManager.ins.language.value;
 
         if (ins.create.changed) {
             const newId = Document.generateId();
@@ -138,8 +129,8 @@ export default class CVAudioTask extends CVTask
 
         if (clip && (ins.title.changed || ins.filepath.changed || ins.captionPath.changed)) {
             clip.name = ins.title.value;
-            clip.uris[ELanguageType[ins.language.value]] = ins.filepath.value;
-            clip.captionUris[ELanguageType[ins.language.value]] = ins.captionPath.value;
+            clip.uris[ELanguageType[activeLanguage]] = ins.filepath.value;
+            clip.captionUris[ELanguageType[activeLanguage]] = ins.captionPath.value;
             audioManager.updateAudioClip(clip.id);
         }
         if (ins.isNarration.changed) {
@@ -172,27 +163,17 @@ export default class CVAudioTask extends CVTask
         const ins = this.ins;
         const audioManager = this.audioManager;
         const clip = audioManager.getAudioClip(ins.activeId.value);
+        const languageManager = this.activeDocument.setup.language;
+        const activeLanguage = languageManager.ins.language.value;
 
         ins.title.setValue(clip ? clip.name : "", true);
-        ins.filepath.setValue(clip ? clip.uris[ELanguageType[ins.language.value]] : "", true);
-        ins.captionPath.setValue(clip ? clip.captionUris[ELanguageType[ins.language.value]] : "", true);
+        ins.filepath.setValue(clip ? clip.uris[ELanguageType[activeLanguage]] : "", true);
+        ins.captionPath.setValue(clip ? clip.captionUris[ELanguageType[activeLanguage]] : "", true);
         ins.isNarration.setValue(clip ? this.audioManager.narrationId === clip.id : false, true);
     }
 
     protected onDocumentLanguageChange()
     {
-        this.synchLanguage();
         this.onAudioChange();
-    }
-
-    // Make sure this task language matches document
-    protected synchLanguage() {
-        const {ins} = this;
-        const languageManager = this.activeDocument.setup.language;
-
-        if(ins.language.value !== languageManager.outs.language.value)
-        {
-            ins.language.setValue(languageManager.outs.language.value, true);
-        }
     }
 }

commit 3b4f64ec9d7ab8fc058957115384446455d2c4a1

Fix for regression from 69b59c8 that throws an error when leaving tasks using LanguagManager

source/client/ui/story/AnnotationsTaskView.ts
@@ -74,6 +74,10 @@ export default class AnnotationsTaskView extends TaskView<CVAnnotationsTask>
 
     protected render()
     {
+        if(!this.activeDocument) {
+            return;
+        }
+
         const node = this.activeNode;
         const annotations = node && node.getComponent(CVAnnotationView, true);
         const languageManager = this.activeDocument.setup.language;

source/client/ui/story/ArticlesTaskView.ts
@@ -46,6 +46,10 @@ export default class ArticlesTaskView extends TaskView<CVArticlesTask>
 
     protected render()
     {
+        if(!this.activeDocument) {
+            return;
+        }
+        
         const task = this.task;
         const articles = task.articles;
         const activeArticle = task.activeArticle;

source/client/ui/story/AudioTaskView.ts
@@ -57,7 +57,7 @@ export default class AudioTaskView extends TaskView<CVAudioTask>
 
     protected render()
     {
-        if(!this.task.audioManager) {
+        if(!this.task.audioManager || !this.activeDocument) {
             return;
         }
         

source/client/ui/story/ToursTaskView.ts
@@ -61,6 +61,10 @@ export default class ToursTaskView extends TaskView<CVToursTask>
     {
         //console.log("TourTaskView.render");
 
+        if(!this.activeDocument) {
+            return;
+        }
+
         const task = this.task;
         const tours = task.tours;
 