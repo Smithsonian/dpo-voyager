bef13b9364ef7a4b52d50d0b24ce6a777bf47e39
Sebastien DUMETZ <s.dumetz@holusion.com>
2024-10-22 15:02:41 +0200

    prevent interacting with properties that have their values linked from another prop

--- source/client/ui/story/PropertyView.ts
+++ source/client/ui/story/PropertyView.ts
@@ -68,32 +68,33 @@ export default class PropertyView extends CustomElement
         const property = this.property;
         const schema = property.schema;
         const label = this.label !== undefined ? this.label : property.path.split(".").pop();
-
+        let linked = this.property.hasMainInLinks();
         if(property.isArray() && property.type !== "number"){
             console.error("Unsupported property : ", property);
             return null;
         }
-
         if(property.type === "number" && property.schema.semantic === "color"){
-            return html`<sv-property-color name=${label} .property=${property}></sv-property-color>`;
+            return html`<sv-property-color ?disabled=${linked} name=${label} .property=${property}></sv-property-color>`;
         }else if (property.type === "number" && property.isArray()) {
             let fields = [];
             for (let i = 0; i < property.elementCount; ++i) {
+                
+                let index_linked = linked || this.property.hasInLinks(i);
                 const fieldLabel = property.schema.labels?.[i] ?? _defaultLabels[i];
-                fields.push(html`<sv-property-number name=${fieldLabel} .index=${i} .property=${property}></sv-property-number>`);
+                fields.push(html`<sv-property-number ?disabled=${index_linked} name=${fieldLabel} .index=${i} .property=${property}></sv-property-number>`);
             }
             const headerElement = label ? html`<div class="sv-property-name">${label}</div>` : null;
             return html`${headerElement}<div class="sv-property-group">${fields}</div>`;
         }else if (schema.event) {
-            return html`<sv-property-event name=${label} .property=${property}></sv-property-event>`;
+            return html`<sv-property-event ?disabled=${linked} name=${label} .property=${property}></sv-property-event>`;
         }else if (schema.options) {
-            return html`<sv-property-options dropdown name=${label} .property=${property}></sv-property-options>`;
+            return html`<sv-property-options ?disabled=${linked} dropdown name=${label} .property=${property}></sv-property-options>`;
         }else if(property.type === "boolean"){
-            return html`<sv-property-boolean name=${label} .property=${property}></sv-property-boolean>`;
+            return html`<sv-property-boolean ?disabled=${linked} name=${label} .property=${property}></sv-property-boolean>`;
         }else if(property.type === "string"){
-            return html`<sv-property-string name=${label} .property=${property}></sv-property-string>`
+            return html`<sv-property-string ?disabled=${linked} name=${label} .property=${property}></sv-property-string>`
         }else if(property.type === "number"){
-            return html`<sv-property-number name=${label} .property=${property}></sv-property-number>`
+            return html`<sv-property-number ?disabled=${linked} name=${label} .property=${property}></sv-property-number>`
         }else{
             console.warn("Unhandled property :", property.name);
             return html`<div class="sv-property-name">${label}</div><div class="sv-property-group">${property.value} (not editable)</div>`;
