77ef72982e9a40a1fe68c883b68cdf954ae7a484
Jonny Bylsma <jbylsma@quotient-inc.com>
2024-11-08 18:27:33 -0500

    Streamline, use conventions for Docker image

---
+++ services/server/setup/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+readonly APP_HOME=/app
+
+# NVM_DIR is set when building the Docker image
+# shellcheck disable=SC1091,SC2154
+source "${NVM_DIR}/nvm.sh"
+
+cd "${APP_HOME}"
+npm install
+
+# build server code in services/server/bin/
+if [[ ! -d "${APP_HOME}/services/server/bin" ]]; then
+  npm run build-server
+fi
+
+# build development client code in dist/
+if [[ ! -d "${APP_HOME}/dist" ]]; then
+  npm run build-dev
+fi
+
+# start server in debug mode
+npm run server
