ea81cd3d004762ee3ac978d5751582d362374d24
Carsten Schnober <c.schnober@esciencecenter.nl>
2025-08-15 11:09:22 +0200

    Add Lights task.

---
+++ source/client/components/CVLightsTask.ts
@@ -0,0 +1,79 @@
+
+import NVNode from "client/nodes/NVNode";
+import LightsTaskView from "client/ui/story/LightsTaskView";
+import CVTask, { types } from "./CVTask";
+import { CLight, ELightType } from "./lights/CVLight";
+
+export default class CVLightsTask extends CVTask {
+    static readonly typeName: string = "CVLightsTask";
+
+    static readonly text: string = "Lights";
+    static readonly icon: string = "bulb";
+
+    protected static readonly ins = {
+        create: types.Event("Light.Create"),
+        delete: types.Event("Light.Delete"),
+        name: types.String("Light.Name", ""),
+        activeId: types.String("Light.ActiveId", ""),
+        type: types.Enum("Light.Type", ELightType, ELightType.directional),
+        // TODO: set color/intensity defaults
+    };
+    protected static readonly outs = {
+    };
+
+    ins = this.addInputs<CVTask, typeof CVLightsTask.ins>(CVLightsTask.ins);
+    outs = this.addOutputs<CVTask, typeof CVLightsTask.outs>(CVLightsTask.outs);
+
+    get lightsList() {
+        return this.system.getComponents(CLight);
+    }
+
+    lightById(id: string): CLight | undefined {
+        return this.lightsList.find(light => light.id === id);
+    }
+
+    create(): void {
+        super.create();
+        this.startObserving();
+    }
+
+    dispose(): void {
+        this.stopObserving();
+        super.dispose();
+    }
+
+    createView() {
+        return new LightsTaskView(this);
+    }
+
+    activateTask(): void {
+        super.activateTask();
+    }
+
+    deactivateTask(): void {
+        super.deactivateTask();
+    }
+
+    update() {
+        const { ins } = this;
+        const light = this.lightById(ins.activeId.value);
+
+        if (light) {
+            if (ins.create.changed) {
+                console.log("create light", ins.type.value);
+            } else if (ins.delete.changed) {
+                light.dispose();
+                let transform = this.system.getComponents("CTransform").find(transform => {
+                    return (transform.node === light.node);
+                });
+                transform?.dispose();
+                // TODO delete navigation panel entry
+            }
+        }
+        return true;
+    }
+
+    protected onActiveNode(previous: NVNode, next: NVNode) {
+        this.ins.activeId.setValue("");
+    }
+}
---
+++ source/client/ui/story/LightsTaskView.ts
@@ -0,0 +1,119 @@
+import List from "client/../../libs/ff-ui/source/List";
+import CVAnnotationView from "client/components/CVAnnotationView";
+import CVLightsTask from "../../components/CVLightsTask";
+import { ILight, TLightType } from "client/schema/document";
+import { TaskView, customElement, html, property } from "../../components/CVTask";
+import CLight from "@ff/scene/components/CLight";
+import { ELightType } from "client/components/lights/CVLight";
+
+@customElement("sv-light-task-view")
+export default class LightsTaskView extends TaskView<CVLightsTask> {
+    protected connected(): void {
+        super.connected();
+        this.task.on("update", this.onUpdate, this);
+    }
+
+    protected disconnected(): void {
+        this.task.off("update", this.onUpdate, this);
+        super.disconnected();
+    }
+
+    protected render() {
+        // if (!this.task.lightManager) {
+        //     return html`<div class="sv-placeholder">No lights available in this scene.</div>`;
+        // }
+
+        const ins = this.task.ins;
+        const lightsList = this.task.lightsList;
+
+
+        const selectedNode = this.nodeProvider.activeNode;
+        let lightElement = this.task.lightById(ins.activeId.value);
+
+        if (selectedNode && selectedNode.light) {
+            lightElement = lightsList.find((light) => light.node.name === selectedNode.name);
+            if (lightElement) {
+                ins.name.setValue(selectedNode.name);
+                // ins.type.setValue(lightElement.constructor.type); // FIXME as ELightType
+                ins.activeId.setValue(lightElement.id);
+            } else {
+                throw new Error("Light not found for selected node");
+            }
+        }
+
+
+        // <sv-property-view .property=${selectedNode.name}></sv-property-view>    
+
+        // <ff-splitter direction="vertical"></ff-splitter>
+        // <div class="ff-flex-row ff-group"><div class="sv-panel-header sv-task-item">Type</div><div class="sv-panel-header sv-task-item sv-item-border-l">Trigger</div></div>
+
+        // TODO add color/intensity properties
+        const detailView = lightElement ? html`<div class="ff-scroll-y ff-flex-column sv-detail-view">
+            <sv-property-view .property=${ins.activeId}></sv-property-view>    
+            <sv-property-view .property=${ins.name}></sv-property-view>    
+            <sv-property-view .property=${ins.type}></sv-property-view>
+        </div>` : null;
+
+        return html`<div class="sv-commands">
+            <ff-button text="Create" icon="create" @click=${this.onClickCreate}></ff-button>
+            <ff-button text="Delete" icon="trash" ?disabled=${!lightElement} @click=${this.onClickDelete}></ff-button>
+        </div>
+        <div class="ff-flex-item-stretch">
+            <div class="ff-flex-column ff-fullsize">
+                <div class="ff-splitter-section" style="flex-basis: 70%">
+                    ${detailView}
+                </div>
+            </div>
+        </div>`;
+    }
+
+    protected onClickCreate() {
+        this.task.ins.create.set();
+    }
+
+    protected onClickDelete() {
+        this.task.ins.delete.set();
+    }
+    protected onSelectAction(event: ISelectLightEvent) {
+        this.task.ins.activeId.setValue(event.detail.light ? event.detail.light.id : "");
+        this.onUpdate();
+    }
+}
+
+interface ISelectLightEvent extends CustomEvent {
+    target: LightList;
+    detail: {
+        light: ILight;
+        index: number;
+    }
+}
+
+@customElement("sv-light-list")
+export class LightList extends List<ILight> {
+    @property({ attribute: false })
+    selectedItem: ILight = null;
+
+    protected firstConnected(): void {
+        super.firstConnected();
+        this.classList.add("sv-group", "sv-light-list");
+    }
+    protected renderItem(item: ILight) {
+        return html`<div class="ff-flex-row ff-group">
+            <div class="sv-task-item">${item.id}</div>
+            <div class="sv-task-item">${item.type}</div>
+        </div>`;
+    }
+    protected isItemSelected(item: ILight): boolean {
+        return this.selectedItem === item;
+    }
+    protected onItemClick(event: MouseEvent, item: ILight, index: number): void {
+        this.dispatchEvent(new CustomEvent("select", {
+            detail: { action: item, index }
+        }));
+    }
+    protected onClickEmpty(event: MouseEvent): void {
+        this.dispatchEvent(new CustomEvent("select", {
+            detail: { action: null, index: -1 }
+        }));
+    }
+}
