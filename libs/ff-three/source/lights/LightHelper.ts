46f09e46e7d90c8415e50171a2e928f39a5070dd
Carsten Schnober <c.schnober@esciencecenter.nl>
2025-08-15 11:33:38 +0200

    Dispose LightHelper if Light has been removed.

--- libs/ff-three/source/lights/LightHelper.ts
+++ libs/ff-three/source/lights/LightHelper.ts
@@ -33,6 +33,11 @@ export default class LightHelper extends Object3D {
   }
 
   update(){
+        if (!this.light.parent) {
+          console.debug("Light has been deleted.")
+          this.dispose();
+          this.source.geometry.dispose(); // FIXME: this does not remove the visible indicator
+        }
   }
 
 	dispose() {
