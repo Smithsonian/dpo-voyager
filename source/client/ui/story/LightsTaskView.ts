1a1ac25e6cd1cb00ee81649ded253112c370e367
Carsten Schnober <c.schnober@esciencecenter.nl>
2025-08-22 16:27:48 +0200

    Remove unused ILight.id, ISelectLightEvent, LightList.

--- source/client/ui/story/LightsTaskView.ts
+++ source/client/ui/story/LightsTaskView.ts
@@ -1,9 +1,7 @@
-import List from "client/../../libs/ff-ui/source/List";
 import { lightTypes } from "client/applications/coreTypes";
 import { ELightType } from "client/components/lights/CVLight";
-import { ILight } from "client/schema/document";
 import CVLightsTask from "../../components/CVLightsTask";
-import { TaskView, customElement, html, property } from "../../components/CVTask";
+import { TaskView, customElement, html } from "../../components/CVTask";
 
 @customElement("sv-light-task-view")
 export default class LightsTaskView extends TaskView<CVLightsTask> {
@@ -52,43 +50,5 @@ export default class LightsTaskView extends TaskView<CVLightsTask> {
 
     protected onClickDelete() {
         this.task.ins.delete.set();
-    }
-}
-
-interface ISelectLightEvent extends CustomEvent {
-    target: LightList;
-    detail: {
-        light: ILight;
-        index: number;
-    }
-}
-
-@customElement("sv-light-list")
-export class LightList extends List<ILight> {
-    @property({ attribute: false })
-    selectedItem: ILight = null;
-
-    protected firstConnected(): void {
-        super.firstConnected();
-        this.classList.add("sv-group", "sv-light-list");
-    }
-    protected renderItem(item: ILight) {
-        return html`<div class="ff-flex-row ff-group">
-            <div class="sv-task-item">${item.id}</div>
-            <div class="sv-task-item">${item.type}</div>
-        </div>`;
-    }
-    protected isItemSelected(item: ILight): boolean {
-        return this.selectedItem === item;
-    }
-    protected onItemClick(event: MouseEvent, item: ILight, index: number): void {
-        this.dispatchEvent(new CustomEvent("select", {
-            detail: { action: item, index }
-        }));
-    }
-    protected onClickEmpty(event: MouseEvent): void {
-        this.dispatchEvent(new CustomEvent("select", {
-            detail: { action: null, index: -1 }
-        }));
     }
 }
