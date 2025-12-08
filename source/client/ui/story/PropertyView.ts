ab8b3cf62aa9f3ffe397e0c9466241cd261412c4
Carsten Schnober <c.schnober@esciencecenter.nl>
2025-12-08 13:52:05 +0100

    Refactor: group all number type properties in one if-clause.

--- source/client/ui/story/PropertyView.ts
+++ source/client/ui/story/PropertyView.ts
@@ -78,18 +78,24 @@ export default class PropertyView extends CustomElement
             console.error("Unsupported property : ", property);
             return null;
         }
-        if(property.type === "number" && property.schema.semantic === "color"){
-            return html`<sv-property-color ?aria-disabled=${disabled} name=${label} .property=${property}></sv-property-color>`;
-        }else if (property.type === "number" && property.isArray()) {
-            let fields = [];
-            for (let i = 0; i < property.elementCount; ++i) {
-                
-                let index_disabled = disabled || this.property.hasInLinks(i);
-                const fieldLabel = property.schema.labels?.[i] ?? _defaultLabels[i];
-                fields.push(html`<sv-property-number aria-disabled=${index_disabled} name=${fieldLabel} .index=${i} .property=${property}></sv-property-number>`);
+        if(property.type === "number"){
+            if (property.schema.semantic === "color") {
+                return html`<sv-property-color ?aria-disabled=${disabled} name=${label} .property=${property}></sv-property-color>`;
+            } else if (property.isArray()) {
+                let fields = [];
+                for (let i = 0; i < property.elementCount; ++i) {
+                    
+                    let index_disabled = disabled || this.property.hasInLinks(i);
+                    const fieldLabel = property.schema.labels?.[i] ?? _defaultLabels[i];
+                    fields.push(html`<sv-property-number aria-disabled=${index_disabled} name=${fieldLabel} .index=${i} .property=${property}></sv-property-number>`);
+                }
+                const headerElement = label ? html`<div class="sv-property-name">${label}</div>` : null;
+                return html`${headerElement}<div class="sv-property-group">${fields}</div>`;
+            } else if (schema.percent){
+                return html`<sv-property-percent aria-disabled=${disabled} name=${label} .property=${property}></sv-property-percent>`
+            } else {
+                return html`<sv-property-number aria-disabled=${disabled} name=${label} .property=${property}></sv-property-number>`
             }
-            const headerElement = label ? html`<div class="sv-property-name">${label}</div>` : null;
-            return html`${headerElement}<div class="sv-property-group">${fields}</div>`;
         }else if (schema.event) {
             return html`<sv-property-event aria-disabled=${disabled} name=${label} .property=${property}></sv-property-event>`;
         }else if (schema.options) {
@@ -98,12 +104,6 @@ export default class PropertyView extends CustomElement
             return html`<sv-property-boolean aria-disabled=${disabled} name=${label} .property=${property}></sv-property-boolean>`;
         }else if(property.type === "string"){
             return html`<sv-property-string aria-disabled=${disabled} name=${label} .property=${property}></sv-property-string>`
-        }else if(property.type === "number"){
-            if(schema.percent){
-                return html`<sv-property-percent aria-disabled=${disabled} name=${label} .property=${property}></sv-property-percent>`
-            }else{
-                return html`<sv-property-number aria-disabled=${disabled} name=${label} .property=${property}></sv-property-number>`
-            }
         }else{
             console.warn("Unhandled property :", property.name);
             return html`<div class="sv-property-name">${label}</div><div class="sv-property-group">${property.value} (not editable)</div>`;
