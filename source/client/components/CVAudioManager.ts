25b27121a978d6e3f185c05090d82a2d687bff23
Jamie Cope <jamie@gcope.com>
2025-12-19 15:19:39 -0500

    Time display reformat and bug fixes.

--- source/client/components/CVAudioManager.ts
+++ source/client/components/CVAudioManager.ts
@@ -212,12 +212,11 @@ export default class CVAudioManager extends Component
     }
 
     setTimeElapsed(time: number) {
-        if(this.audioPlayer) { 
+        if(this.audioPlayer && this.audioView) { 
             if(this.audioPlayer.seekable.length === 0) {
                 this.audioPlayer.addEventListener("canplay",() => this.setTimeElapsed(time), {once: true});
             }      
             else {
-                //console.log(this.audioPlayer.seekable.start(0)+" "+this.audioPlayer.seekable.end(0));
                 this.audioPlayer.currentTime = time;
                 this.audioView.elapsed = time;
                 this.audioView.requestUpdate();
@@ -336,7 +335,7 @@ export default class CVAudioManager extends Component
         } 
         this.outs.isPlaying.setValue(false);
         this.audioPlayer.pause();
-        this.audioView.requestUpdate();
+        this.audioView?.requestUpdate();
     }
 
     stop()
@@ -354,7 +353,7 @@ export default class CVAudioManager extends Component
         
         this.isPlaying = false;
         outs.isPlaying.setValue(false);
-        this.audioView.requestUpdate();
+        this.audioView?.requestUpdate();
     }
 
     // Initialize player for a specific audio clip
@@ -369,21 +368,23 @@ export default class CVAudioManager extends Component
             if(this.audioPlayer.src != this._audioMap[uri]) {
                 this.audioPlayer.setAttribute("src", this._audioMap[uri]);
                 //this.audioPlayer.load();
+            }
 
-                // Set caption track source
-                const captionUri = clip.captionUris[ELanguageType[this.language.outs.activeLanguage.getValidatedValue()] as TLanguageType];
-                if(captionUri) {
-                    if(this.audioPlayer.children[0]) {
-                        this.audioPlayer.children[0].remove();
-                    }
+            // Set caption track source
+            const captionUri = clip.captionUris[ELanguageType[this.language.outs.activeLanguage.getValidatedValue()] as TLanguageType];
+            if(captionUri && (this.audioPlayer.children.length == 0 || 
+                (this.audioPlayer.children[0] as HTMLTrackElement).src != this.assetManager.getAssetUrl(captionUri))) {
 
-                    const textTrack = document.createElement('track');
-                    this.audioPlayer.append(textTrack);
-                    textTrack.setAttribute("src", this.assetManager.getAssetUrl(captionUri));
-                    textTrack.track.mode = "showing";
-                    textTrack.addEventListener("cuechange", this.onCueChange);
-                    textTrack.addEventListener("load", this.onLoadTrack);
+                if(this.audioPlayer.children[0]) {
+                    this.audioPlayer.children[0].remove();
                 }
+
+                const textTrack = document.createElement('track');
+                this.audioPlayer.append(textTrack);
+                textTrack.setAttribute("src", this.assetManager.getAssetUrl(captionUri));
+                textTrack.track.mode = "showing";
+                textTrack.addEventListener("cuechange", this.onCueChange);
+                textTrack.addEventListener("load", this.onLoadTrack);
             }
         }
     }
@@ -428,8 +429,10 @@ export default class CVAudioManager extends Component
     // Handle audio time elapsed updates
     protected onTimeChange = (event: Event) =>
     {
-        this.audioView.elapsed = this.getTimeElapsed();
-        this.audioView.requestUpdate();
+        if(this.audioView) {
+            this.audioView.elapsed = this.getTimeElapsed();
+            this.audioView.requestUpdate();
+        }
     }
 
     protected onLanguageChange() {
@@ -528,7 +531,7 @@ export class AudioView extends CustomElement
     protected formatSeconds(seconds: number) {
         var date = new Date(0);
         date.setSeconds(seconds);
-        var formatString = date.toISOString().substring(15, 19);
+        var formatString = date.toISOString().substring(14, 19);
         return formatString;
     }
 }
