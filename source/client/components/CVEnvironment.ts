acd043beeb2c973a9ccf37c389c0047c6bc50bb8
Carsten Schnober <c.schnober@esciencecenter.nl>
2026-02-23 12:34:40 +0100

    Make scanForEnvironmentImages() to include files in sub-directories.

--- source/client/components/CVEnvironment.ts
+++ source/client/components/CVEnvironment.ts
@@ -98,7 +98,7 @@ export default class CVEnvironment extends Component
         if (assetManager) {
             assetManager.on<IAssetTreeChangeEvent>("tree-change", this.onAssetTreeChange, this);
             if (assetManager.root) {
-                this.scanForEnvironmentImages();
+                this.scanForEnvironmentImages(assetManager.root);
             }
         }
     }
@@ -307,11 +307,15 @@ export default class CVEnvironment extends Component
     }
 
     protected onAssetTreeChange(event: IAssetTreeChangeEvent) {
-        this.scanForEnvironmentImages();
+        this.scanForEnvironmentImages(this.assetManager.root);
     }
 
-    protected scanForEnvironmentImages() {
-        this.assetManager.root.children
+    protected scanForEnvironmentImages(root: IAssetEntry) {
+        root.children
+            .filter(entry => entry.info.folder)
+            .forEach(folder => this.scanForEnvironmentImages(folder));
+        
+        root.children
             .map(entry => entry.info.path)
             .filter(path => path.toLowerCase().endsWith(".hdr"))
             .filter(path => !this._imageOptions.includes(path))
