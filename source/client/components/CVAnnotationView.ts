9edde87d539d9f6cf9ace71fc0f5d2e267cd5016
Nicolas Dejardin <nico@spatiodata.com>
2026-03-13 11:13:22 +0100

    Consolidate annotation activation behavior into CVAnnotationView
    All side-effects of activating an annotation (camera tween to saved view,
    auto-rotation stop) are now handled in the `set activeAnnotation` setter,
    ensuring consistent behavior regardless of activation path (click, keyboard, API).
    
    Removes `focusAnnotation` from CVViewer and MainView, which was added to work
    around the missing behavior on the `setActiveAnnotation` API path.

--- source/client/components/CVAnnotationView.ts
+++ source/client/components/CVAnnotationView.ts
@@ -46,6 +46,7 @@ import CVAssetReader from "./CVAssetReader";
 import CVAudioManager from "./CVAudioManager";
 import CVAssetManager from "./CVAssetManager";
 import CVSnapshots from "./CVSnapshots";
+import CVOrbitNavigation from "./CVOrbitNavigation";
 import CPulse from "client/../../libs/ff-graph/source/components/CPulse";
 import CVScene from "./CVScene";
 
@@ -159,6 +160,30 @@ export default class CVAnnotationView extends CObject3D
                 if(this._activeView) {
                     this._truncateLock = true;
                     this._activeView = false;
+                }
+
+                // start view animation if it exists
+                if (annotation.data.viewId.length && !this.arManager.outs.isPresenting.value) {
+
+                    // stop auto-rotation when an annotation is activated
+                    const navigation = this.getGraphComponent(CVOrbitNavigation, true);
+                    if (navigation) {
+                        navigation.ins.autoRotation.setValue(false);
+                        navigation.ins.promptActive.setValue(false);
+                    }
+
+                    this.normalizeViewOrbit(annotation.data.viewId);
+
+                    // If activeAnnotation is being tracked, make sure it is set
+                    const activeIdx = this.snapshots.getTargetProperties().findIndex(prop => prop.name == "ActiveId");
+                    if (activeIdx >= 0) {
+                        const viewState = this.snapshots.getState(annotation.data.viewId);
+                        viewState.values[activeIdx] = annotation.data.id;
+                    }
+
+                    const pulse = this.getMainComponent(CPulse);
+                    this.snapshots.tweenTo(annotation.data.viewId, pulse.context.secondsElapsed);
+                    this._activeView = true;
                 }
             }
 
@@ -516,23 +541,6 @@ export default class CVAnnotationView extends CObject3D
     protected onSpriteClick(event: any)
     {
         this.emit(event);
-
-        // start view animation if it exists
-        const annotation = event.annotation;
-        if(annotation && annotation.data.viewId.length && !this.arManager.outs.isPresenting.value) {
-            this.normalizeViewOrbit(annotation.data.viewId);
-
-            // If activeAnnotation is being tracked, make sure it is set
-            const activeIdx = this.snapshots.getTargetProperties().findIndex(prop => prop.name == "ActiveId");
-            if(activeIdx >= 0) {
-                const viewState = this.snapshots.getState(annotation.data.viewId);
-                viewState.values[activeIdx] = annotation.data.id;
-            }
-            
-            const pulse = this.getMainComponent(CPulse);
-            this.snapshots.tweenTo(annotation.data.viewId, pulse.context.secondsElapsed);
-            this._activeView = true;
-        }
     }
 
     protected onSpriteLink(event: any)
